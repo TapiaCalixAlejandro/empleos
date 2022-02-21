675c8836471de5c9e959f5e3567cd7ce631dc220
TapiaCalixAlejandro <tapiacalix6@gmail.com>
2022-02-20 19:07:23 -0600

    Modelo Category

--- database/migrations/2022_02_20_005801_create_categories_table.php
+++ database/migrations/2022_02_20_005801_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVacantsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateVacantsTable extends Migration
      */
     public function up()
     {
-        Schema::create('vacants', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->onDelete('cascade')->onUpdate('cascade');            
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ class CreateVacantsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('vacants');
+        Schema::dropIfExists('categories');
     }
 }
